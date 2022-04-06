c5af94e04e71c01067da0443fe0dc837c0c95d39
sidsaik <77861140+sidsaik@users.noreply.github.com>
2022-04-05 22:40:59 -0400

    Add files via upload

--- PersonDetection/human detection.py
+++ PersonDetection/human detection.py
@@ -3,13 +3,13 @@ import numpy as np
 import os
 
 # initialize HOG descriptor
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hogDetect = cv2.HOGDescriptor()
+hogDetect.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 cv2.startWindowThread()
 
 # connect to camera
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 pixelWidth=1
 width=45
@@ -26,12 +26,12 @@ gray = cv2.cvtColor(distanceImage, cv2.COLOR_RGB2GRAY)
 
 
 # return bounding boxes
-boxes, weights = hog.detectMultiScale(distanceImage, winStride=(8,8) )
+b, w = hogDetect.detectMultiScale(distanceImage, winStride=(8,8) )
 
-boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+b = np.array([[x, y, x + w, y + h] for (x, y, w, h) in b])
 
 count=0
-for (xLeft, yLeft, xRight, yRight) in boxes:
+for (xLeft, yLeft, xRight, yRight) in b:
     # output box on the image
     cv2.rectangle(distanceImage, (xLeft, yLeft), (xRight, yRight),
                       (0, 255, 0), 2)
@@ -47,20 +47,20 @@ focalLength = (imageWidth * distance) / width
 
 while(True):
     #Read in frames from camera
-    ret, frame = cap.read()
+    ret, frame = capture.read()
 
-    # resize image
+    # resize frame
     frame = cv2.resize(frame, (640, 480))
     # use a grey image
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     # return bounding boxes
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+    b, w = hogDetect.detectMultiScale(frame, winStride=(8,8) )
 
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    b = np.array([[x, y, x + w, y + h] for (x, y, w, h) in b])
 
     count=0
-    for (xLeft, yLeft, xRight, yRight) in boxes:
+    for (xLeft, yLeft, xRight, yRight) in b:
         # display the detected boxes in the colour picture
         cv2.rectangle(frame, (xLeft, yLeft), (xRight, yRight),
                           (0, 255, 0), 2)
@@ -75,12 +75,11 @@ while(True):
     #print("PixelWidth: ",pixelWidth)
     if frameDistance!=0:
         print("Distance:",frameDistance)
-    #cv2.putText(frame, f'P{count}', (xLeft, yLeft), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 
-cap.release()
+capture.release()
 cv2.destroyLeftllWindows()
